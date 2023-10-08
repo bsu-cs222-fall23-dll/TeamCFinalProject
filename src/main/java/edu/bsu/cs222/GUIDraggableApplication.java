f114bd3bd661cc951eb99a115f04631857603318
Joshua Miller <joshua.miller3@bsu.edu>
2023-10-08 15:06:21 -0400

    Created Base Draggable GUI
    Signed-off-by: JMiller921387 <122392455+JMiller921387@users.noreply.github.com>

---
+++ src/main/java/edu/bsu/cs222/GUIDraggableApplication.java
@@ -0,0 +1,72 @@
+package edu.bsu.cs222;
+
+import javafx.application.Application;
+import javafx.scene.Cursor;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
+
+
+public class GUIDraggableApplication extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        Node circle = new Circle(25, 25, 25, Color.YELLOW);
+        Node rect = new Rectangle(150, 30, Color.RED);
+        Label control = new Label("Hello World");
+        control.setFont(Font.font(42));
+
+        Node[] nodeArray = new Node[]{ circle, rect, control};
+
+        stage.setScene(new Scene(createDraggableApp(nodeArray)));
+        stage.show();
+    }
+
+    public Pane createPane(int x, int y, Node[] content){
+        Pane pane = new Pane(content);
+        pane.setPrefSize(x, y);
+        return pane;
+    }
+
+
+    public Parent createDraggableApp(Node[] nodeArray) {
+
+        for(int i = 0; i<nodeArray.length; i++){
+            nodeArray[i].setTranslateX(i * 75);
+            nodeArray[i].setTranslateY(50);
+        }
+        var root = createPane(800, 600, nodeArray);
+
+        root.getChildren().forEach(this::makeDraggable);
+
+        return root;
+    }
+
+    private double startX;
+    private double startY;
+
+    private void makeDraggable(Node node){
+        node.setOnMousePressed(obj -> {
+            startX = obj.getSceneX() - node.getTranslateX();
+            startY = obj.getSceneY() - node.getTranslateY();
+
+            node.setCursor(Cursor.CLOSED_HAND);
+        });
+
+        node.setOnMouseDragged(obj -> {
+            node.setTranslateX(obj.getSceneX() - startX);
+            node.setTranslateY(obj.getSceneY() - startY);
+        });
+
+    }
+
+
+
+}
